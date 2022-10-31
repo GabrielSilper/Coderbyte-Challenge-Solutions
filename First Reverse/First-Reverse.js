022ebd7366b6f2daff2db5bc04aff815117e7ebf
Gabriel Silper <2019003726@ifam.edu.br>
2022-10-30 22:21:26 -0400

    First Reverse Challenge

---
+++ First Reverse/First-Reverse.js
@@ -0,0 +1,10 @@
+function FirstReverse(str) { 
+    // code goes here  
+    // str = str.split('');
+    // str.reverse();
+    // str = str.join('');
+    return str.split('').reverse().join('');
+  }
+     
+  // keep this function call here 
+  console.log(FirstReverse('Abcd'));
